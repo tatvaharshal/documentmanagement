b7bef884279385666c734cf503a105517f46732a
bhavinsharda <bhawin12@gecg28.ac.in>
2017-05-03 18:54:55 +0530

    initial changes bhavin

--- KendoPractice/src/main/java/com/tatvacoconet/controller/DocumentDataController.java
+++ KendoPractice/src/main/java/com/tatvacoconet/controller/DocumentDataController.java
@@ -5,20 +5,17 @@ import java.util.Arrays;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
+
+import com.tatvacoconet.dto.DocumentDTO;
+import com.tatvacoconet.dto.MasterDocumentDTO;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.tatvacoconet.dto.DocumentDTO;
 import com.tatvacoconet.entity.DocumentData;
 import com.tatvacoconet.entity.DocumentLink;
 import com.tatvacoconet.service.DocumentDataService;
@@ -48,7 +45,7 @@ import com.tatvacoconet.service.DocumentLinkService;
 	/*                      List all Method 					*/
 
         @RequestMapping(value = "/documentList", method = RequestMethod.GET, headers = "Accept=application/json")
-        public ResponseEntity<List<DocumentDTO>> getAllDocuments(Model model){
+        public ResponseEntity<List<DocumentDTO>> getAllDocuments(){
             // Retrieve all documents
             List<DocumentData> document = dataService.getAllDocs();
 
@@ -58,20 +55,8 @@ import com.tatvacoconet.service.DocumentLinkService;
             for (DocumentData doc : document) {
                 // Create new data transfer object
                 DocumentDTO dto = new DocumentDTO();
-                dto.setDocumentId(doc.getDocumentId());
-                dto.setDocumentName(doc.getDocumentName());
-                dto.setDocumentDescription(doc.getDocumentDescription());
-                dto.setFilePath(doc.getFilePath());
-                dto.setFileSize(doc.getFileSize());
-                dto.setDocumentStatus(doc.getDocumentStatus());
-                dto.setDocumentTag(doc.getDocumentTag());
-                dto.setDocumentType(doc.getDocumentType());
-                dto.setCreationDate(doc.getCreationDate());
-                dto.setValidFrom(doc.getValidFrom());
-                dto.setValidTo(doc.getValidTo());
-                dto.setVerticalData(doc.getVerticalData());
-                dto.setDocumentLink(linkService.getAllDocumentsLink(doc
-                        .getDocumentId()));
+                dto.setDocumentData(doc);
+                dto.setDocumentLink(linkService.getAllDocumentsLink(doc.getDocumentId()));
                 // Add to model list
                 documentDTO.add(dto);
             }
@@ -90,81 +75,147 @@ import com.tatvacoconet.service.DocumentLinkService;
         }
 	/* 							   Add Method 							      	*/
 
-        @RequestMapping(value = "/addDocument", method = RequestMethod.POST, headers = "Accept=application/json")
-        public ResponseEntity<DocumentData> addDocument(@RequestBody DocumentData document,
-                                                        @ModelAttribute DocumentLink documentlink1,
-                                                        HttpSession session){
-            // public String postAdd(@ModelAttribute("documentAttribute") DocumentData document,HttpSession session) {
-            logger.debug("Received request to add new record");
-            session.removeAttribute("documentid");
-
-            DocumentData document1 = (DocumentData) session.getAttribute("document1");
-            if (document1 != null){
-                document.setFileSize(document1.getFileSize());
-                document.setFilePath(document1.getFilePath());
-                document.setDocumentDescription(document1.getDocumentDescription());
-                document.setDocumentName(document1.getDocumentName());
-            }
-
-            // Delegate to service
-            dataService.RegisterOrUpdate(document);
-
-            DocumentData doc = document;
-
-            documentlink1.setDocumentData(doc);
-
-            DocumentLink documentlink2 = documentlink1;
-            session.setAttribute("documentid", documentlink2);
-
-            session.removeAttribute("document1");
-
-            logger.debug("Added:: " + document);
-            return new ResponseEntity<DocumentData>(document, HttpStatus.CREATED);
+    @RequestMapping(value = "/addDocument", method = RequestMethod.POST, headers = "Accept=application/json")
+    public ResponseEntity<MasterDocumentDTO> addDocument(@RequestBody MasterDocumentDTO masterDocumentDTO1,
+                                                         @ModelAttribute DocumentData document,
+                                                         @ModelAttribute DocumentLink document_link,
+                                                         HttpSession session, Model model){
+        // public String postAdd(@ModelAttribute("documentAttribute") Document_Data document,HttpSession session) {
+        logger.debug("Received request to add new record");
+        DocumentData document2 = (DocumentData) session.getAttribute("document1");
+        if (document2 != null){
+            document.setFileSize(document2.getFileSize());
+            document.setFilePath(document2.getFilePath());
+            document.setDocumentDescription(document2.getDocumentDescription());
+            document.setDocumentName(document2.getDocumentName());
         }
+
+
+        DocumentData document1=new DocumentData();
+
+        document1=document;
+        document1.setCreationDate(masterDocumentDTO1.getCreationDate());
+        document1.setValidFrom(masterDocumentDTO1.getValidFrom());
+        document1.setValidTo(masterDocumentDTO1.getValidTo());
+        document1.setDocumentStatus(masterDocumentDTO1.getDocumentStatus());
+        document1.setDocumentTag(masterDocumentDTO1.getDocumentTag());
+        document1.setDocumentType(masterDocumentDTO1.getDocumentType());
+        document1.setVerticalData(masterDocumentDTO1.getVerticalData());
+
+        if(document1!=null)
+        {
+            dataService.RegisterOrUpdate(document1);
+            session.removeAttribute("document1");
+        }
+
+        DocumentLink documentLink1=new DocumentLink();
+
+        documentLink1.setFirstName(masterDocumentDTO1.getFirstName());
+        documentLink1.setLastName(masterDocumentDTO1.getLastName());
+        documentLink1.setGroupDetails(masterDocumentDTO1.getGroupDetails());
+        documentLink1.setRoleDetails(masterDocumentDTO1.getRoleDetails());
+        documentLink1.setUserId(masterDocumentDTO1.getUserId());
+
+        if(documentLink1!=null)
+        {	documentLink1.setDocumentData(document1);
+            linkService.addDocument(documentLink1);
+
+        }
+        logger.debug("Added:: " + document);
+        return new ResponseEntity<MasterDocumentDTO>(masterDocumentDTO1, HttpStatus.CREATED);
+    }
 
 	/* 							Add File Method 						 	*/
 
-        @RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
-        @Produces("application/json")
-        @Consumes("application/pdf")
-        public ResponseEntity<DocumentData> uploadDocument(@RequestParam("file")
-                                                                   MultipartFile multipartFile, HttpSession session){
+    @RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
+    @Produces("application/json")
+    @Consumes("application/pdf")
+    public ResponseEntity<DocumentData> uploadDocument(@RequestParam("file") MultipartFile multipartFile,HttpSession session){
 	/* @RequestBody Document_Data document */
-            DocumentData document1 = new DocumentData();
-
-
+        DocumentData document1 = new DocumentData();
 		/* null the session */
-            session.removeAttribute("document1");
-
+        session.removeAttribute("document1");
 		/* Fill the values into session */
+        if (!multipartFile.isEmpty()) {
+            try {
+                document1.setDocumentName(multipartFile.getOriginalFilename());
+                document1.setDocumentDescription(multipartFile.getContentType());
+                document1.setFileSize(multipartFile.getSize());
+                document1.setFilePath(multipartFile.getBytes());
 
-            if (!multipartFile.isEmpty()) {
-                try {
-				/*String originalFilename = multipartFile.getOriginalFilename();
-				 File destinationFile = new File(
-				  servletContext.getRealPath("/resources/files/") +
-				  originalFilename); multipartFile.transferTo(destinationFile);
-				 */
-                    document1.setDocumentName(multipartFile.getOriginalFilename());
-                    document1.setDocumentDescription(multipartFile.getContentType());
-                    document1.setFileSize(multipartFile.getSize());
-                    document1.setFilePath(multipartFile.getBytes());
+                session.setAttribute("document1", document1);
 
-                    session.setAttribute("document1", document1);
+                logger.debug("Added:: " + document1);
 
-                    logger.debug("Added:: " + document1);
-
-                    return new ResponseEntity<DocumentData>(document1,HttpStatus.CREATED);
-                }
-                catch (Exception e){
-                    return new ResponseEntity<DocumentData>(HttpStatus.BAD_REQUEST);
-                }
+                return new ResponseEntity<DocumentData>(document1,HttpStatus.CREATED);
             }
-            else {
+            catch (Exception e){
                 return new ResponseEntity<DocumentData>(HttpStatus.BAD_REQUEST);
             }
+        }
+        else {
+            return new ResponseEntity<DocumentData>(HttpStatus.BAD_REQUEST);
+        }
+    }
+    /**
+     * Retrieves the "Edit Existing Record" page
+     */
+    @RequestMapping(value = "/documentListByID/{documentId}", method = RequestMethod.GET, headers = "Accept=application/json")
+    public ResponseEntity<DocumentDTO> getDocumentsByID(@PathVariable("documentId")long documentId){
+        // Retrieve all documents by  id
+
+        DocumentDTO documentDTO=new DocumentDTO();
+        documentDTO.setDocumentData(dataService.getDocumentByID(documentId));
+        documentDTO.setDocumentLink(linkService.getAllDocumentsLink(documentId));
+        logger.debug("Added:: " + documentDTO);
+        return new ResponseEntity<DocumentDTO>(documentDTO, HttpStatus.CREATED);
+
+    }
+    @RequestMapping(value="/updateDocument", method = RequestMethod.PUT, headers="Accept=application/json")
+    public void updateDocument(@RequestBody MasterDocumentDTO masterDocumentDTO1, @ModelAttribute DocumentData document,@ModelAttribute DocumentLink documentLink)
+    {
+        document.setDocumentId(masterDocumentDTO1.getDocumentId());
+        document.setDocumentDescription(masterDocumentDTO1.getDocumentDescription());
+        document.setDocumentName(masterDocumentDTO1.getDocumentName());
+        document.setDocumentStatus(masterDocumentDTO1.getDocumentStatus());
+        document.setDocumentTag(masterDocumentDTO1.getDocumentTag());
+        document.setCreationDate(masterDocumentDTO1.getCreationDate());
+        document.setValidFrom(masterDocumentDTO1.getValidFrom());
+        document.setValidTo(masterDocumentDTO1.getValidTo());
+        document.setDocumentType(masterDocumentDTO1.getDocumentType());
+        document.setVerticalData(masterDocumentDTO1.getVerticalData());
+
+        if(document!=null)
+        {	dataService.updateDocument(document);
 
         }
+        documentLink.setDocumentLinkId(masterDocumentDTO1.getDocumentLinkId());
+        documentLink.setFirstName(masterDocumentDTO1.getFirstName());
+        documentLink.setLastName(masterDocumentDTO1.getLastName());
+        documentLink.setGroupDetails(masterDocumentDTO1.getGroupDetails());
+        documentLink.setRoleDetails(masterDocumentDTO1.getRoleDetails());
+        documentLink.setUserId(masterDocumentDTO1.getUserId());
+        documentLink.setDocumentData(document);
+        if(documentLink!=null)
+        {
+            //document_link.setDocument_data(document);
 
+            linkService.updateDocumentLink(documentLink);
+        }
+    }
+    @RequestMapping(value = "/deleteDocument/{documentId}", method = RequestMethod.GET)
+    public ResponseEntity<DocumentData> getDelete(@PathVariable("documentId")long documentId) {
+        logger.debug("Received request to delete record");
+
+        // Delete all associated credit cards first
+        linkService.deleteAll(documentId);
+
+        // Delete person
+        dataService.delete(documentId);
+
+        // Redirect to url
+        return new ResponseEntity<DocumentData>(HttpStatus.NO_CONTENT);
+
+    }
     }
 
