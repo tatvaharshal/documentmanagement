e73fec75eea6a0e24e1003f9189d6f204aeecac4
bhavinsharda <bhawin12@gecg28.ac.in>
2017-05-03 18:56:19 +0530

    initial changes bhavin

--- KendoPractice/src/main/java/com/tatvacoconet/repository/DocumentDataRepository.java
+++ KendoPractice/src/main/java/com/tatvacoconet/repository/DocumentDataRepository.java
@@ -3,8 +3,13 @@ package com.tatvacoconet.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tatvacoconet.entity.DocumentData;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface DocumentDataRepository extends JpaRepository<DocumentData, Long>{
 
-
+    @Query( "select creationDate,documentDescription,documentName,documentStatus,documentTag,documentType,fileSize,validFrom,validTo,verticalData from DocumentData where documentId=:documentId")
+    List<DocumentData> findByID(@Param("documentId") long documentId);
 }
