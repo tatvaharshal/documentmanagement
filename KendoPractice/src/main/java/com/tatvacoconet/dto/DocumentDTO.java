513c76746256748540dc942967b320ee01a706ee
bhavinsharda <bhawin12@gecg28.ac.in>
2017-05-04 14:43:07 +0530

    initial changes bhavin

--- KendoPractice/src/main/java/com/tatvacoconet/dto/DocumentDTO.java
+++ KendoPractice/src/main/java/com/tatvacoconet/dto/DocumentDTO.java
@@ -1,123 +1,24 @@
 package com.tatvacoconet.dto;
 
-import java.util.Date;
-import java.util.List;
-
-import com.tatvacoconet.entity.DocumentStatus;
-import com.tatvacoconet.entity.DocumentTag;
-import com.tatvacoconet.entity.DocumentType;
-import com.tatvacoconet.entity.VerticalData;
+import com.tatvacoconet.entity.DocumentData;
 import com.tatvacoconet.entity.DocumentLink;
 
+import java.util.List;
+
+/**
+ * Created by pca48 on 5/3/2017.
+ */
 public class DocumentDTO {
-    private long documentId;
-    private String documentName;
-    private String documentDescription;
-    private byte[] filePath;
-    private long fileSize;
-    private Date creationDate;
-    private Date validFrom;
-    private Date validTo;
-    private VerticalData verticalData;
-    private DocumentStatus documentStatus;
-    private DocumentType documentType;
-    private DocumentTag documentTag;
+
+    private DocumentData documentData;
     private List<DocumentLink> documentLink;
 
-    public long getDocumentId() {
-        return documentId;
+    public DocumentData getDocumentData() {
+        return documentData;
     }
 
-    public void setDocumentId(long documentId) {
-        this.documentId = documentId;
-    }
-
-    public String getDocumentName() {
-        return documentName;
-    }
-
-    public void setDocumentName(String documentName) {
-        this.documentName = documentName;
-    }
-
-    public String getDocumentDescription() {
-        return documentDescription;
-    }
-
-    public void setDocumentDescription(String documentDescription) {
-        this.documentDescription = documentDescription;
-    }
-
-    public byte[] getFilePath() {
-        return filePath;
-    }
-
-    public void setFilePath(byte[] filePath) {
-        this.filePath = filePath;
-    }
-
-    public long getFileSize() {
-        return fileSize;
-    }
-
-    public void setFileSize(long fileSize) {
-        this.fileSize = fileSize;
-    }
-
-    public Date getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public Date getValidFrom() {
-        return validFrom;
-    }
-
-    public void setValidFrom(Date validFrom) {
-        this.validFrom = validFrom;
-    }
-
-    public Date getValidTo() {
-        return validTo;
-    }
-
-    public void setValidTo(Date validTo) {
-        this.validTo = validTo;
-    }
-
-    public VerticalData getVerticalData() {
-        return verticalData;
-    }
-
-    public void setVerticalData(VerticalData verticalData) {
-        this.verticalData = verticalData;
-    }
-
-    public DocumentStatus getDocumentStatus() {
-        return documentStatus;
-    }
-
-    public void setDocumentStatus(DocumentStatus documentStatus) {
-        this.documentStatus = documentStatus;
-    }
-
-    public DocumentType getDocumentType() {
-        return documentType;
-    }
-
-    public void setDocumentType(DocumentType documentType) {
-        this.documentType = documentType;
-    }
-
-    public DocumentTag getDocumentTag() {
-        return documentTag;
-    }
-
-    public void setDocumentTag(DocumentTag documentTag) {
-        this.documentTag = documentTag;
+    public void setDocumentData(DocumentData documentData) {
+        this.documentData = documentData;
     }
 
     public List<DocumentLink> getDocumentLink() {
