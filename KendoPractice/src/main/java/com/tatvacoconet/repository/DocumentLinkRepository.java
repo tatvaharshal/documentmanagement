daffdb18e3a5f1fce9c51bb338bc5766d11d886b
bhavinsharda <bhawin12@gecg28.ac.in>
2017-05-04 16:08:55 +0530

    initial changes bhavin

--- KendoPractice/src/main/java/com/tatvacoconet/repository/DocumentLinkRepository.java
+++ KendoPractice/src/main/java/com/tatvacoconet/repository/DocumentLinkRepository.java
@@ -2,6 +2,7 @@ package com.tatvacoconet.repository;
 
 import java.util.List;
 
+import com.tatvacoconet.entity.DocumentData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,9 +12,9 @@ import com.tatvacoconet.entity.DocumentLink;
 public interface DocumentLinkRepository extends JpaRepository<DocumentLink, Long>{
 
     //@Query("Select documentLinkId,firstName,groupDetails,lastName,roleDetails,userId from DocumentLink where documentData_documentId=documentData_documentId")
-    @Query( "select documentLinkId,firstName,groupDetails,lastName,roleDetails,userId from DocumentLink where documentData_documentId in :documentId")
-    List<DocumentLink> findByID(@Param("documentId")long documentId);
-
+    @Query("select documentLinkId,firstName,groupDetails,lastName,roleDetails,userId from DocumentLink where documentData.documentId in:documentId")
+    List<DocumentLink> findByID(@Param("documentId") long documentId);
+   // List<DocumentLink> findByDocumentID(DocumentData documentData);
 
 
 }
